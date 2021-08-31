56b198da5472b565b5a094a4f757c9cf5fe6d64d
Elimar7 <89210381+Elimar7@users.noreply.github.com>
2021-08-30 23:23:41 -0700

    Add files via upload

---
+++ prework-1.py
@@ -0,0 +1,18 @@
+user_name = "jimmihen"
+print("Hello, " + user_name.title() + "!")
+
+n=0
+while(n<=100) :
+    if n%2!=0 :
+     print(n, end=" ")
+    n=n+1
+
+digits = [1, 2, 3, 4, 5, 0]
+max(digits)
+
+e=0
+while(e<=20) :
+    print(e, end=" ")
+    e=e+1
+
+ # a little confused on the leap year question
